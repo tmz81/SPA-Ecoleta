305ae0d9cb178247b554b035976af654e0b6dda8
tmz81 <tmz_81@hotmail.com>
2021-09-17 20:04:41 -0300

    Started update nunjucks 🚀

--- src/server.js
+++ src/server.js
@@ -11,9 +11,18 @@ server.use(express.static('public'))
 server.use(express.urlencoded({ extended: true }))
 
 //template engine
+const nujucks = require('nujucks')
+nujucks.configure('src/views', {
+  express: server,
+  noCache: true
+})
 
 //config route
-server.get('/', (req, res) => res.render('index.html'))
+server.get('/', (req, res) => res.render('index.html', {title: 'Um title'}))
+
+server.get('/create-point', (req, res) => res.render('create-point.html'))
+
+server.get('/search', (req, res) => res.render('search-results.html'))
 
 //on server
 server.listen(3333)

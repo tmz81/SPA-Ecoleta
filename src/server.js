488895e351f520e38d14fa40768cf26853ce3bb2
tmz81 <tmz_81@hotmail.com>
2021-09-17 23:02:07 -0300

    🚀

--- src/server.js
+++ src/server.js
@@ -2,27 +2,85 @@ const express = require('express');
 const server = express();
 
 //get db
-
+const db = require('./db/db')
 
 //config static public
-server.use(express.static('public'))
+server.use(express.static("public"))
 
 //use red.body
 server.use(express.urlencoded({ extended: true }))
 
 //template engine
-const nujucks = require('nujucks')
-nujucks.configure('src/views', {
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
   express: server,
   noCache: true
 })
 
 //config route
-server.get('/', (req, res) => res.render('index.html', {title: 'Um title'}))
+server.get('/', (req, res) => {
+  return res.render("index.html", {title: 'Um title'})
+})
 
-server.get('/create-point', (req, res) => res.render('create-point.html'))
+server.get('/create-point.html', (req, res) => {
+  return res.render('create-point.html')
+})
 
-server.get('/search', (req, res) => res.render('search-results.html'))
+server.post("/savepoint", (req, res) => {
+  const query = `
+  INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+  ) VALUES (?,?,?,?,?,?,?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err){
+    if(err) {
+      console.log(err)
+      return res.send("Erro no cadastro!")
+    }
+
+    console.log('Cadastro com sucesso!')
+    console.log(this)
+
+    return res.render("create-point.html", {saved: true})
+  }
+
+  db.run(query, values, afterInsertData)
+})
+
+server.get("/search", (req, res) => {
+  const search = req.query.search
+
+  if(search == ""){
+    return res.render("search-results.html", {  total:0 })
+  }
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+    if(err){
+      return console.log(err)
+    }
+
+    const total = rows.length
+
+    return res.render("search-results.html", {  places: rows, total: total})
+  })
+})
 
 //on server
 server.listen(3333)
